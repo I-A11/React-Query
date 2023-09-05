d926ced1815f190493754e1e71e5b2c0d770e542
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-09-05 10:41:11 +1000

    Use query helper functions to fetch all tasks at screen without refresh.

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,12 +1,23 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 import customFetch from "./utilis";
+import { toast } from "react-toastify";
 
 const Form = () => {
   const [newItemName, setNewItemName] = useState("");
+  const queryClient = useQueryClient();
 
   const { mutate: createTask, isLoading } = useMutation({
     mutationFn: (taskTitle) => customFetch.post("/", { title: taskTitle }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+      toast.success("task added");
+      setNewItemName("");
+    },
+    onError: (error) => {
+      console.log(error);
+      toast.error(error.response.data.msg);
+    },
   });
 
   const handleSubmit = (e) => {
